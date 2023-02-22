4ad6d50e907fce1cf79d2867a33afb0db30732e5
maxxii22 <123008433+maxxii22@users.noreply.github.com>
2023-02-22 17:16:45 +0100

    commit initial files

---
+++ index.js
@@ -0,0 +1,44 @@
+scEl = document.getElementById("sc1")
+scEll = document.getElementById("sc2")
+newGame = document.getElementById("newG")
+let score = 0
+function add1() {
+    score += 1
+    scEl.textContent = score
+}
+function add1() {
+    score += 1
+    scEl.textContent = score
+}
+function add2() {
+    score += 2
+    scEl.textContent = score
+}
+function add3() {
+    score += 3
+    scEl.textContent = score
+}
+let scores = 0
+function add4() {
+    scores += 1
+    scEll.textContent = scores
+}
+function add5() {
+    scores += 2
+    scEll.textContent = scores
+}
+function add6() {
+    scores += 3
+    scEll.textContent = scores
+}
+function newG() {
+    score = 0
+    scores = 0
+    scEl.textContent = score
+    scEll.textContent = scores
+    
+}const txt = `
+
+And t
+
+`
